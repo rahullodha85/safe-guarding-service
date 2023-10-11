146183f5816a3b9d7e93e4999f177057e5782adb
Rahul Lodha <rahullodha@141.sub-97-32-66.myvzw.com>
2023-10-11 12:20:41 -0400

    db schema

--- dbmigration.py
+++ dbmigration.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Integer, Date, Boolean
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Integer, Date, Boolean, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.testing.schema import Column
 from sqlalchemy.orm import sessionmaker
 
@@ -10,6 +10,24 @@ engine.connect()
 Base = declarative_base()
 
 
+class Patient(Base):
+    __tablename__ = "patient"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+
+
+class Employee(Base):
+    __tablename__ = "employee"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+
+
 class Appointment(Base):
     __tablename__ = "appointment"
 
@@ -18,6 +36,10 @@ class Appointment(Base):
     end_time = Column(Date)
     employee_arrived = Column(Boolean, default=False)
     employee_departed = Column(Boolean, default=False)
+    employee_id = Column(Integer, ForeignKey('employee.id'))
+    employee = relationship("employee", back_populates="appointment")
+    patient_id = Column(Integer, ForeignKey('patient.id'))
+    patient = relationship("patient", back_populates="appointment")
 
 
 Base.metadata.create_all(engine)
