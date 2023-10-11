e585dee4c2e0c4a19a6be78dd806cdd343f51c4f
Syeda Arzoo Irshad <arzooirshad7@gmail.com>
2023-10-11 12:50:26 -0400

    add create endpoint

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# In-memory database (for demonstration purposes)
+items = []
+
+# Pydantic model for item data
+class Item(BaseModel):
+    name: str
+    description: str
+
+# Create an item
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    items.append(item)
+    return item
